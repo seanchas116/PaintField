625f899527d01f20f10afe10640913094322141a
iofg2100 <iofg2100@gmail.com>
2013-01-16 22:33:35 +0900

    Fix: Linux deployment script

--- linux_deploy.sh
+++ linux_deploy.sh
@@ -3,7 +3,7 @@
 DEST=$2/PaintField
 
 if [ -e $DEST ]; then
-rm -r $DEST
+  rm -r $DEST
 fi
 
 mkdir $DEST
@@ -12,10 +12,10 @@ cp $1/src/app/PaintField $DEST
 cp $1/src/app/paintfield-launch.sh $DEST
 cp -r $1/src/app/Settings $DEST
 cp -r $1/src/app/Contents $DEST
-cp $1/src/libs/Malachite/src/lib*.so.* $DEST
-cp $1/src/libs/Minizip/lib*.so.* $DEST
-cp $1/src/paintfield-core/lib*.so.* $DEST
-cp $1/src/paintfield-extension/lib*.so.* $DEST
+cp $1/src/libs/Malachite/src/lib*.so.1 $DEST
+cp $1/src/libs/Minizip/lib*.so.1 $DEST
+cp $1/src/paintfield-core/lib*.so.1 $DEST
+cp $1/src/paintfield-extension/lib*.so.1 $DEST
 
 chmod +x $DEST/paintfield-launch.sh
 
