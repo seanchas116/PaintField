221f497ce5541f4143338b1f661732a181f31f7c
iofg2100 <iofg2100@gmail.com>
2013-03-17 17:48:50 +0900

    fixed random string

--- src/paintfield/core/randomstring.cpp
+++ src/paintfield/core/randomstring.cpp
@@ -10,15 +10,14 @@ QString randomString(int length)
 {
 	qsrand(time(0));
 	
-	QByteArray source;
-	int sourceLength = length * 6 / 8 + 1;
-	source.reserve(sourceLength);
-	for (int i = 0; i < sourceLength; ++i) {
-		source += uint8_t(qrand() % 256);
+	QString str;
+	
+	for (int i = 0; i < length; ++i)
+	{
+		str += QString::number(qrand() % 36, 36);
 	}
 	
-	QString result(source.toBase64());
-	return result.left(length);
+	return str;
 }
 
 QString createTemporaryFilePath()
