14fa7212e5b9bfe420fa84887cbe412a15a53f33
iofg2100 <iofg2100@gmail.com>
2013-07-19 01:05:05 +0900

    update translations

--- src/paintfield/core/core_ja.ts
+++ src/paintfield/core/core_ja.ts
@@ -150,6 +150,99 @@
     </message>
 </context>
 <context>
+    <name>PaintField::BlendModeTexts</name>
+    <message>
+        <location filename="blendmodetexts.cpp" line="16"/>
+        <source>Normal</source>
+        <translation>通常</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="17"/>
+        <source>Plus</source>
+        <translation>加算</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="18"/>
+        <source>Multiply</source>
+        <translation>乗算</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="19"/>
+        <source>Screen</source>
+        <translation>スクリーン</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="20"/>
+        <source>Overlay</source>
+        <translation>オーバーレイ</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="21"/>
+        <source>Darken</source>
+        <translation>比較 (暗)</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="22"/>
+        <source>Lighten</source>
+        <translation>比較 (明)</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="23"/>
+        <source>Color Dodge</source>
+        <translation>覆い焼き</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="24"/>
+        <source>Color Burn</source>
+        <translation>焼き込み</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="25"/>
+        <source>Hard Light</source>
+        <translation>ハードライト</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="26"/>
+        <source>Soft Light</source>
+        <translation>ソフトライト</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="27"/>
+        <source>Difference</source>
+        <translation>差の絶対値</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="28"/>
+        <source>Exclusion</source>
+        <translation>除外</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="29"/>
+        <source>Hue</source>
+        <translation>色相</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="30"/>
+        <source>Saturation</source>
+        <translation>彩度</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="31"/>
+        <source>Color</source>
+        <translation>色</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="32"/>
+        <source>Luminosity</source>
+        <translation>明度</translation>
+    </message>
+    <message>
+        <location filename="blendmodetexts.cpp" line="34"/>
+        <source>Pass Through</source>
+        <translation>パススルー</translation>
+    </message>
+</context>
+<context>
     <name>PaintField::ColorButton</name>
     <message>
         <location filename="widgets/colorbutton.cpp" line="33"/>
@@ -265,38 +358,38 @@
 <context>
     <name>PaintField::LayerScene</name>
     <message>
-        <location filename="layerscene.cpp" line="683"/>
+        <location filename="layerscene.cpp" line="693"/>
         <source>Remove Layers</source>
         <translation>レイヤーを削除</translation>
     </message>
     <message>
-        <location filename="layerscene.cpp" line="701"/>
-        <location filename="layerscene.cpp" line="727"/>
+        <location filename="layerscene.cpp" line="711"/>
+        <location filename="layerscene.cpp" line="737"/>
         <source>Move Layers</source>
         <translation>レイヤーを移動</translation>
     </message>
     <message>
-        <location filename="layerscene.cpp" line="756"/>
+        <location filename="layerscene.cpp" line="766"/>
         <source>Merge Layers</source>
         <translation>レイヤーを統合</translation>
     </message>
     <message>
-        <location filename="layerscene.cpp" line="803"/>
+        <location filename="layerscene.cpp" line="813"/>
         <source>Rename Layer</source>
         <translation>レイヤーの名前を変更</translation>
     </message>
     <message>
-        <location filename="layerscene.cpp" line="806"/>
+        <location filename="layerscene.cpp" line="816"/>
         <source>Change visibility</source>
         <translation>表示・非表示を変更</translation>
     </message>
     <message>
-        <location filename="layerscene.cpp" line="809"/>
+        <location filename="layerscene.cpp" line="819"/>
         <source>Change Blend Mode</source>
         <translation>ブレンドモードを変更</translation>
     </message>
     <message>
-        <location filename="layerscene.cpp" line="812"/>
+        <location filename="layerscene.cpp" line="822"/>
         <source>Change Opacity</source>
         <translation>不透明度を変更</translation>
     </message>
@@ -304,22 +397,22 @@
 <context>
     <name>PaintField::NewDocumentDialog</name>
     <message>
-        <location filename="dialogs/newdocumentdialog.cpp" line="26"/>
+        <location filename="dialogs/newdocumentdialog.cpp" line="29"/>
         <source>New Document</source>
         <translation>新規書類</translation>
     </message>
     <message>
-        <location filename="dialogs/newdocumentdialog.cpp" line="37"/>
+        <location filename="dialogs/newdocumentdialog.cpp" line="40"/>
         <source>Width</source>
         <translation>幅</translation>
     </message>
     <message>
-        <location filename="dialogs/newdocumentdialog.cpp" line="42"/>
+        <location filename="dialogs/newdocumentdialog.cpp" line="45"/>
         <source>Height</source>
         <translation>高さ</translation>
     </message>
     <message>
-        <location filename="dialogs/newdocumentdialog.cpp" line="45"/>
+        <location filename="dialogs/newdocumentdialog.cpp" line="48"/>
         <source>Keep Aspect Ratio</source>
         <translation>縦横比を保持</translation>
     </message>
