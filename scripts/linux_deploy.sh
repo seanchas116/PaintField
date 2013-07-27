3462a6551468ae5e774002bd6aed8c43dae04c99
iofg2100 <iofg2100@gmail.com>
2013-07-26 22:32:55 -0700

    fix linux_deploy

--- scripts/linux_deploy.sh
+++ scripts/linux_deploy.sh
@@ -13,9 +13,9 @@ cp $SRC/src/paintfield/app/PaintField $DEST
 cp -r $SRC/src/paintfield/app/Settings $DEST
 cp -r $SRC/src/paintfield/app/Contents $DEST
 cp -r $SRC/src/paintfield/app/Translations $DEST
-cp $SRC/src/libs/Malachite/src/lib*.so.1 $DEST
-cp $SRC/src/libs/minizip/lib*.so.1 $DEST
-cp $SRC/src/libs/qtsingleapplication/lib*.so.1 $DEST
-cp $SRC/src/paintfield/core/lib*.so.1 $DEST
-cp $SRC/src/paintfield/extensions/lib*.so.1 $DEST
+cp -P $SRC/src/libs/Malachite/src/lib*.so* $DEST
+cp -P $SRC/src/libs/minizip/lib*.so* $DEST
+cp -P $SRC/src/libs/qtsingleapplication/lib*.so* $DEST
+cp -P $SRC/src/paintfield/core/lib*.so* $DEST
+cp -P $SRC/src/paintfield/extensions/lib*.so* $DEST
 
