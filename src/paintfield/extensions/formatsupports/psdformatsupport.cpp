c93031347e6727c3bfad8ffc518fcf4d5b0f08c3
Ryohei Ikegami <iofg2100@gmail.com>
2013-08-25 23:54:09 +0900

    add PSD read support

--- src/paintfield/extensions/formatsupports/psdformatsupport.cpp
+++ src/paintfield/extensions/formatsupports/psdformatsupport.cpp
@@ -3,6 +3,10 @@
 #include <type_traits>
 #include <memory>
 #include <QStack>
+#include <QHash>
+#include <boost/range/adaptor/reversed.hpp>
+#include "paintfield/core/rasterlayer.h"
+#include "paintfield/core/grouplayer.h"
 
 #include "psdformatsupport.h"
 
@@ -14,6 +18,11 @@ public:
 
 	BinaryStream(QIODevice *device) :
 		m_dataStream(device)
+	{
+	}
+
+	BinaryStream(QByteArray *byteArray, QIODevice::OpenMode mode) :
+		m_dataStream(byteArray, mode)
 	{
 	}
 
@@ -202,15 +211,82 @@ struct PsdImageResourceSection
 	}
 };
 
+struct PsdChannelData
+{
+	uint16_t compression = 1;
+	QByteArray rawData;
+
+	void load(BinaryStream &stream, const QRect &rect, int depth, int dataSize)
+	{
+		stream.pushPos();
+
+		int byteDepth = depth / 8;
+
+		int width = rect.width();
+		int height = rect.height();
+		int byteCount = width * height * byteDepth;
+
+		stream >> compression;
+
+		switch (compression)
+		{
+			case 0: // no compression
+			{
+				PAINTFIELD_DEBUG << "no compression";
+				rawData = stream.read(byteCount);
+				PAINTFIELD_DEBUG << "actual data size" << rawData.size();
+				break;
+			}
+			case 1: // PackBits
+			{
+				PAINTFIELD_DEBUG << "PackBits";
+
+				rawData.reserve(byteCount);
+
+				QVector<uint16_t> srcScanlineLengths;
+				srcScanlineLengths.reserve(height);
+
+				int dstScanlineLength = width * byteDepth;
+
+				for (int y = 0; y < height; ++y)
+				{
+					uint16_t length;
+					stream >> length;
+					srcScanlineLengths << length;
+				}
+
+				for (int y = 0; y < height; ++y)
+				{
+					auto srcData = stream.read(srcScanlineLengths[y]);
+					rawData += decodePackBits(srcData, dstScanlineLength);
+				}
+
+				PAINTFIELD_DEBUG << "expected data size" << byteCount;
+				PAINTFIELD_DEBUG << "actual data size" << rawData.size();
+
+				break;
+			}
+			default:
+				throw std::runtime_error("unsupported compression");
+		}
+
+		stream.popPos();
+		stream.move(dataSize);
+	}
+};
+
+
 struct PsdChannelInfo
 {
-	uint16_t id;
+	int16_t id;
 	uint32_t length;
 
 	void load(BinaryStream &stream)
 	{
 		stream >> id;
+		PAINTFIELD_DEBUG << "id" << id;
 		stream >> length;
+		PAINTFIELD_DEBUG << "length" << length;
 	}
 };
 
@@ -255,6 +331,27 @@ struct PsdLayerRecord
 	uint32_t sectionType = 0;
 	QByteArray sectionBlendModeKey;
 
+	QVector<Ref<PsdChannelData>> channelDatas;
+
+	enum struct Flag
+	{
+		TransparencyProtected = 1 << 0,
+		Visible = 1 << 1
+	};
+
+	enum struct SectionType
+	{
+		Other = 0,
+		OpenFolder = 1,
+		ClosedFolder = 2,
+		BoundingSectionDivider = 3
+	};
+
+	QRect getRect() const
+	{
+		return QRect(rectLeft, rectTop, rectRight - rectLeft, rectBottom - rectTop);
+	}
+
 	void load(BinaryStream &stream)
 	{
 		PAINTFIELD_DEBUG;
@@ -272,6 +369,7 @@ struct PsdLayerRecord
 			PsdChannelInfo info;
 			info.load(stream);
 			channelInfos << info;
+			PAINTFIELD_DEBUG << "channel id" << info.id << "length" << info.length;
 		}
 
 		auto blendModeSignature = stream.read(4);
@@ -348,89 +446,10 @@ struct PsdLayerRecord
 	}
 };
 
-struct PsdChannelData
-{
-	uint16_t compression = 1;
-	QByteArray rawData;
-
-	void load(BinaryStream &stream, const QRect &rect, int depth, int dataSize)
-	{
-		stream.pushPos();
-
-		int byteDepth = 1;
-
-		switch (depth)
-		{
-			case 8:
-				byteDepth = 1;
-				break;
-			case 16:
-				byteDepth = 2;
-				break;
-			case 32:
-				byteDepth = 4;
-				break;
-			default:
-				throw std::runtime_error("unsupported depth");
-		}
-
-		int width = rect.width();
-		int height = rect.height();
-		int byteCount = width * height * byteDepth;
-
-		stream >> compression;
-
-		switch (compression)
-		{
-			case 0: // no compression
-			{
-				PAINTFIELD_DEBUG << "no compression";
-				rawData = stream.read(byteCount);
-				break;
-			}
-			case 1: // PackBits
-			{
-				PAINTFIELD_DEBUG << "PackBits";
-
-				rawData.reserve(byteCount);
-
-				QVector<uint16_t> srcScanlineLengths;
-				srcScanlineLengths.reserve(height);
-
-				int dstScanlineLength = width * byteDepth;
-
-				for (int y = 0; y < height; ++y)
-				{
-					uint16_t length;
-					stream >> length;
-					srcScanlineLengths << length;
-				}
-
-				for (int y = 0; y < height; ++y)
-				{
-					auto srcData = stream.read(srcScanlineLengths[y]);
-					rawData += decodePackBits(srcData, dstScanlineLength);
-				}
-
-				PAINTFIELD_DEBUG << "expected data size" << byteCount;
-				PAINTFIELD_DEBUG << "actual data size" << rawData.size();
-
-				break;
-			}
-			default:
-				throw std::runtime_error("unsupported compression");
-		}
-
-		stream.popPos();
-		stream.move(dataSize);
-	}
-};
-
 struct PsdLayerInfo
 {
 	QList<Ref<PsdLayerRecord>> layerRecords;
 	int16_t signedLayerCount;
-	QList<Ref<PsdChannelData>> channelDataList;
 
 	void load(BinaryStream &stream, int depth)
 	{
@@ -444,6 +463,7 @@ struct PsdLayerInfo
 
 		layerRecords.reserve(layerCount);
 
+		// read layer records
 		for (int i = 0; i < layerCount; ++i)
 		{
 			auto record = std::make_shared<PsdLayerRecord>();
@@ -451,18 +471,17 @@ struct PsdLayerInfo
 			layerRecords << record;
 		}
 
-		for (int i = 0; i < layerCount; ++i)
+		// read channel data
+		for (const auto &layerRecord : layerRecords)
 		{
-			const auto &layerRecord = layerRecords[i];
-			auto channelCount = layerRecord->channelCount;
-			QRect rect(layerRecord->rectLeft, layerRecord->rectTop, layerRecord->rectRight - layerRecord->rectLeft, layerRecord->rectBottom - layerRecord->rectTop);
+			auto rect = layerRecord->getRect();
 
-			for (int channel = 0; channel < channelCount; ++channel)
+			for (const auto &channelInfo : layerRecord->channelInfos)
 			{
 				auto channelData = std::make_shared<PsdChannelData>();
-				auto size = layerRecords[i]->channelInfos[channel].length;
-				channelData->load(stream, rect, depth, size);
-				channelDataList << channelData;
+				channelData->load(stream, rect, depth, channelInfo.length);
+
+				layerRecord->channelDatas << channelData;
 			}
 		}
 	}
@@ -481,6 +500,176 @@ struct PsdLayerAndMaskInformationSection
 		layerInfo.load(stream, depth);
 	}
 };
+
+
+static QHash<QByteArray, Malachite::BlendMode> makeHashToBlendMode()
+{
+	QHash<QByteArray, Malachite::BlendMode> hash;
+	hash["pass"] = Malachite::BlendMode::PassThrough;
+	hash["norm"] = Malachite::BlendMode::Normal;
+	hash["dark"] = Malachite::BlendMode::Darken;
+	hash["mul "] = Malachite::BlendMode::Multiply;
+	hash["idiv"] = Malachite::BlendMode::ColorBurn;
+	hash["lite"] = Malachite::BlendMode::Lighten;
+	hash["scrn"] = Malachite::BlendMode::Screen;
+	hash["div "] = Malachite::BlendMode::ColorDodge;
+	hash["over"] = Malachite::BlendMode::Overlay;
+	hash["sLit"] = Malachite::BlendMode::SoftLight;
+	hash["hLit"] = Malachite::BlendMode::HardLight;
+	hash["diff"] = Malachite::BlendMode::Difference;
+	hash["smud"] = Malachite::BlendMode::Exclusion;
+	hash["hue "] = Malachite::BlendMode::Hue;
+	hash["sat "] = Malachite::BlendMode::Saturation;
+	hash["lum "] = Malachite::BlendMode::Luminosity;
+	return hash;
+}
+
+static auto hashToBlendMode = makeHashToBlendMode();
+
+class PsdSurfaceLoader
+{
+public:
+
+	PsdSurfaceLoader(const QVector<Ref<PsdChannelData>> &channeldDataList, const QVector<PsdChannelInfo> &channelInfos, const QRect &rect, int bpp) :
+		channeldDataList(channeldDataList),
+		channelInfos(channelInfos),
+		rect(rect),
+		bpp(bpp)
+	{}
+
+	Malachite::Surface load() const
+	{
+		Q_ASSERT(channeldDataList.size() == channelInfos.size());
+
+		int count = rect.width() * rect.height();
+		Malachite::Image image(rect.size());
+
+		auto channelDataI = channeldDataList.begin();
+
+		for (const PsdChannelInfo &info : channelInfos)
+		{
+			PAINTFIELD_DEBUG << "image size" << image.area() << "data size" << (*channelDataI)->rawData.size();
+			loadDataForBppAndChannel(image, (*channelDataI)->rawData, bpp, info.id);
+			++channelDataI;
+		}
+
+		// premultiply pixels
+		{
+			auto p = image.bits();
+			for (int i = 0; i < count; ++i)
+			{
+				auto a = p->a();
+				Malachite::Pixel multiplier(1, a, a, a);
+				p->rv() *= multiplier.v();
+				++p;
+			}
+		}
+
+		Malachite::Surface surface;
+		surface.paste(image, rect.topLeft());
+		return surface;
+	}
+
+private:
+
+	template <int bpp>
+	static float loadComponent(const uint8_t *data);
+
+	template <int channel>
+	static void setComponent(Malachite::Pixel *pixel, float value);
+
+	template <int bpp, int channel>
+	static void loadData(Malachite::Image &image, const QByteArray &data)
+	{
+		auto pixelP = image.bits();
+		auto dataP = reinterpret_cast<const uint8_t *>(data.data());
+		int count = image.area();
+
+		if (count != data.size())
+			throw std::runtime_error("data size wrong");
+
+		while (count--)
+		{
+			auto value = loadComponent<bpp>(dataP);
+			setComponent<channel>(pixelP, value);
+			pixelP++;
+			dataP += bpp / 8;
+		}
+	}
+
+	template <int bpp>
+	static void loadDataForChannel(Malachite::Image &image, const QByteArray &data, int psdChannel)
+	{
+		switch (psdChannel)
+		{
+			case -1:
+				return loadData<bpp, Malachite::Pixel::Index::A>(image, data);
+			case 0:
+				return loadData<bpp, Malachite::Pixel::Index::R>(image, data);
+			case 1:
+				return loadData<bpp, Malachite::Pixel::Index::G>(image, data);
+			case 2:
+				return loadData<bpp, Malachite::Pixel::Index::B>(image, data);
+			default:
+				throw std::runtime_error("unsupported channel");
+		}
+	}
+
+	static void loadDataForBppAndChannel(Malachite::Image &image, const QByteArray &data, int bpp, int psdChannel)
+	{
+		switch (bpp)
+		{
+			case 8:
+				return loadDataForChannel<8>(image, data, psdChannel);
+			case 16:
+				return loadDataForChannel<16>(image, data, psdChannel);
+			case 32:
+				return loadDataForChannel<32>(image, data, psdChannel);
+			default:
+				throw std::runtime_error("unsupported bpp");
+		}
+	}
+
+	QVector<Ref<PsdChannelData>> channeldDataList;
+	QVector<PsdChannelInfo> channelInfos;
+	QRect rect;
+	int bpp;
+};
+
+template <> float PsdSurfaceLoader::loadComponent<8>(const uint8_t *data)
+{
+	return data[0] / float(0xFFU);
+}
+
+template <> float PsdSurfaceLoader::loadComponent<16>(const uint8_t *data)
+{
+	return (data[0] << 8U | data[1]) / float(0xFFFFU);
+}
+
+template <> float PsdSurfaceLoader::loadComponent<32>(const uint8_t *data)
+{
+	return (data[0] << 24U | data[1] << 16U | data[2] << 8U | data[3]) / float(0xFFFFFFFFU);
+}
+
+template <> void PsdSurfaceLoader::setComponent<Malachite::Pixel::Index::A>(Malachite::Pixel *pixel, float value)
+{
+	pixel->setA(value);
+}
+
+template <> void PsdSurfaceLoader::setComponent<Malachite::Pixel::Index::R>(Malachite::Pixel *pixel, float value)
+{
+	pixel->setR(value);
+}
+
+template <> void PsdSurfaceLoader::setComponent<Malachite::Pixel::Index::G>(Malachite::Pixel *pixel, float value)
+{
+	pixel->setG(value);
+}
+
+template <> void PsdSurfaceLoader::setComponent<Malachite::Pixel::Index::B>(Malachite::Pixel *pixel, float value)
+{
+	pixel->setB(value);
+}
 
 PsdFormatSupport::PsdFormatSupport(QObject *parent) :
 	FormatSupport(parent)
@@ -505,14 +694,83 @@ bool PsdFormatSupport::read(QIODevice *device, QList<LayerRef> *layers, QSize *s
 
 		PsdLayerAndMaskInformationSection layerSection;
 		layerSection.load(stream, header.depth);
+
+		QStack<LayerRef> parentStack;
+		parentStack.push(std::make_shared<GroupLayer>());
+
+		auto layerRecords = layerSection.layerInfo.layerRecords;
+
+		auto makeLayer = [&](const Ref<PsdLayerRecord> &layerRecord, bool isGroup)
+		{
+			LayerRef layer;
+
+			if (isGroup)
+			{
+				layer = std::make_shared<GroupLayer>();
+			}
+			else
+			{
+				layer = std::make_shared<RasterLayer>();
+			}
+
+			layer->setName(layerRecord->layerName);
+			layer->setOpacity(layerRecord->opacity / 255.f);
+
+			//layer->setOpacityProtected(layerRecord->flags & PsdLayerRecord::Flag::TransparencyProtected);
+			layer->setVisible(!(layerRecord->flags & (int)PsdLayerRecord::Flag::Visible));
+
+			layer->setBlendMode(hashToBlendMode.value(layerRecord->blendModeKey, Malachite::BlendMode::Normal));
+
+			if (!isGroup)
+			{
+				auto rasterLayer = std::static_pointer_cast<RasterLayer>(layer);
+
+				PsdSurfaceLoader surfaceLoader(layerRecord->channelDatas, layerRecord->channelInfos , layerRecord->getRect(), header.depth);
+				rasterLayer->setSurface(surfaceLoader.load());
+			}
+
+			return layer;
+		};
+
+		for (const auto &layerRecord : layerRecords | boost::adaptors::reversed)
+		{
+			switch (layerRecord->sectionType)
+			{
+				case (int)PsdLayerRecord::SectionType::Other:
+				{
+					auto layer = makeLayer(layerRecord, false);
+					parentStack.top()->append(layer);
+					break;
+				}
+				case (int)PsdLayerRecord::SectionType::OpenFolder:
+				case (int)PsdLayerRecord::SectionType::ClosedFolder:
+				{
+					auto group = makeLayer(layerRecord, true);
+					parentStack.push(group);
+					break;
+				}
+				case (int)PsdLayerRecord::SectionType::BoundingSectionDivider:
+				{
+					if (parentStack.size() <= 1)
+						throw std::runtime_error("too many close folder");
+					auto group = parentStack.pop();
+					parentStack.top()->append(group);
+					break;
+				}
+				default:
+					break;
+			}
+		}
+
+		*layers = parentStack.first()->takeAll();
+		*size = QSize(header.width, header.height);
+		return true;
 	}
 	catch (const std::runtime_error &error)
 	{
 		PAINTFIELD_WARNING << error.what();
 		return false;
 	}
-	
-	return false;
 }
 
 bool PsdFormatSupport::write(QIODevice *device, const QList<LayerConstRef> &layers, const QSize &size, const QVariant &option)
